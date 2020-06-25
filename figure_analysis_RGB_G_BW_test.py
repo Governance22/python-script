9301797a77b4e20fcf4618b62f042590cb95c44f
Governance22 <governance22@gmail.com>
2020-06-26 01:40:24 +0800

    Update figure_analysis_RGB_G_BW_test.py

--- figure_analysis_RGB_G_BW_test.py
+++ figure_analysis_RGB_G_BW_test.py
@@ -18,6 +18,6 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
 # In[2]
-blackAndWhiteImageS = cv2.resize(blackAndWhiteImage, ( x, y))                    # Resize image, ratio of width : x, ratio of height : y
+blackAndWhiteImageS = cv2.resize(blackAndWhiteImage, ( x, y))                    # Resize image, ratio of width : x, ratio of height : y => fit in your screen
 cv2.imshow('Result', blackAndWhiteImageS)
 cv2.waitKey(0)                                                                   # Display the image infinitely until any keypress
